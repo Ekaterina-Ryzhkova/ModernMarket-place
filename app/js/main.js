be6ab4e4a48519a70845cba58ea44e18c272a94b
Ekaterina Ryzhkova <January1Paradise@yandex.ru>
2021-12-21 18:49:09 +0500

    Added slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,11 @@ $(function(){
 
     var mixer = mixitup('.products__inner-box');
 
+    $('.product-slider__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
 });
